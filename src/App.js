0c95a2583cf1448925df101b7c929ee5698b9525
ridhoajibx <ridhoajibx@gmail.com>
2021-10-21 16:40:13 +0700

    [ridho] - Initial commit of my components test -WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "./assets/scss/styles.scss";
+import Header from "./components/Headers";
 
 function App() {
   return (
     <div className="App">
+        <Header />
       Testing React with Jest & Enzyme
     </div>
   );
