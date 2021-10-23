1cd7970d11db9655c2e46cbe58e991792d37030c
ridhoajibx <ridhoajibx@gmail.com>
2021-10-23 11:08:38 +0700

    [ridho] - Header component test refactor - WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./assets/scss/styles.scss";
 import Header from "./components/Headers";
+import Headline from "./components/Headline";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <section className="main">
+                <Headline title="Posts" desc="Click the button to render posts" />
+            </section>
         </div>
     );
 }
